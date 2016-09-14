d168c7f30e602861ecbeb6bd21694fb39a01a88b
elavretskiy <e.lavretskiy@gmail.com>
2016-09-14 03:01:20 +0300

    Initial commit

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -3,10 +3,10 @@ require 'rufus-scheduler'
 scheduler = Rufus::Scheduler.new
 
 scheduler.cron '10 0 * * 1' do
-  Article.where(user_id: nil).destroy_all
+  Article.unscoped.where(user_id: nil).destroy_all
 
-  Comment.where(user_id: nil).destroy_all
-  Comment.where(commentable_id: nil).destroy_all
+  Comment.unscoped.where(user_id: nil).destroy_all
+  Comment.unscoped.where(commentable_id: nil).destroy_all
 
   Tagging.where(article_id: nil).destroy_all
   Tagging.where(tag_id: nil).destroy_all
